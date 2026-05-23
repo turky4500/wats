1d42094cbbefab1dbcb0c65ac7298871f5cf21da
turky4500 <tur100@gmail.com>
2026-05-23 11:07:23 +0300

    Update Settings.js

--- models/Settings.js
+++ models/Settings.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const settingsSchema = new mongoose.Schema({
     supportPhone: { type: String, default: '966598686902' },
-    freeTrialDays: { type: Number, default: 2 }
+    freeTrialDays: { type: Number, default: 2 },
+    isSafeMode: { type: Boolean, default: false },
+    isQuietHours: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Settings', settingsSchema);
